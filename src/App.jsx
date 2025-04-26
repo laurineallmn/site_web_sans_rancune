e40a3d31aa435bed0ba090c4a8e1c93a3407f556
Laurine <laurine.allemon@gmail.com>
2025-04-26 14:36:02 +0200

    Fichier d'installation projet
    installation serveur Vite etc.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>SANS RANCUNE</h1>
+    
+      <button onClick={() => afficherVideo()}>
+        VISIONNER ET JOUER 
+      </button>
+    </div>
+  )
+}
+
+
